98e1089cb59017f7ab6e6c85e103bf58caeb962a
monocy <c763416559@163.com>
2020-10-20 10:36:59 +0800

    添加 bind 实现方法

---
+++ bind.js
@@ -0,0 +1,29 @@
+/**
+ * 实现 bind 函数
+ */
+(function () {
+  Function.prototype.myBind = function (...args) {
+    // 原函数对象
+    const self = this;
+    // this 上下文
+    const context = args[0] || null;
+    // 原函数参数
+    const originArgs = args.slice(1);
+
+    // 返回一个新函数
+    return function (...newArgs) {
+      return self.apply(context, [...originArgs, ...newArgs]);
+    };
+  };
+
+  // 实例
+  const obj = { name: 'Smiley' };
+  function greeting(str, lang) {
+    this.value = 'greetingValue';
+    console.log('Welcome ' + this.name + ' to ' + str + ' in ' + lang);
+  }
+  const bindGretting = greeting.myBind(obj, 'the world');
+
+  bindGretting('JS');
+  console.log(obj);
+})();
